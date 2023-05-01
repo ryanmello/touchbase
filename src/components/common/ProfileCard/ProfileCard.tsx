f824e7193da3826946d84c1c28f9e311c4198a25
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-05-01 09:36:24 -0700

    input fields

--- src/components/common/ProfileCard/ProfileCard.tsx
+++ src/components/common/ProfileCard/ProfileCard.tsx
@@ -9,11 +9,9 @@ const ProfileCard = ({ currentUser, onEdit }) => {
   return (
     <div className="profile-card">
       <div className="profile-card-container">
-        <div className="edit-btn-container">
-          <button className="edit-btn" onClick={onEdit}>
-            Edit
-          </button>
-        </div>
+        <button className="edit-btn" onClick={onEdit}>
+          Edit
+        </button>
         <h3 className="username">{currentUser.name}</h3>
         <p className="email">{currentUser.email}</p>
       </div>
--- src/components/common/ProfileEdit/ProfileEdit.jsx
+++ src/components/common/ProfileEdit/ProfileEdit.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import './ProfileEdit.scss'
+import React from "react";
+import "./ProfileEdit.scss";
 
 const ProfileEdit = ({ onEdit }) => {
-  return (
-    <div>
-      Profile Edit
-      <button className='back-btn' onClick={onEdit}>Back</button>
-    </div>
-  )
-}
+  const getInput = (event) => {
 
-export default ProfileEdit
+  };
+
+  return (
+    <div className="profile-edit">
+      <div className="profile-edit-container">
+        <button className="back-btn" onClick={onEdit}>
+          Back
+        </button>
+
+        <h2>Edit Profile</h2>
+        <div className="profile-edit-inputs">
+          <input
+            placeholder="Name"
+            className="common-input"
+            name="name"
+            onChange={getInput}
+          ></input>
+          <input
+            placeholder="About"
+            className="common-input"
+            name="about"
+            onChange={getInput}
+          ></input>
+          <input
+            placeholder="Location"
+            className="common-input"
+            name="location"
+            onChange={getInput}
+          ></input>
+          <input
+            placeholder="Company"
+            className="common-input"
+            name="company"
+            onChange={getInput}
+          ></input>
+          <input
+            placeholder="College"
+            className="common-input"
+            name="college"
+            onChange={getInput}
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileEdit;
